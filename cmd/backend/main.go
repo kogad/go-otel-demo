41292ae70b9880820ed7827c075ca89cd748b07b
kogad <44827429+kogad@users.noreply.github.com>
2025-04-27 23:53:05 +0900

    add

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net"
+	"time"
 
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	pb "example.com/go-otel-demo/proto"
 	"example.com/go-otel-demo/telemetry"
+)
+
+const name = "example.com/go-otel-demo"
+
+var (
+	tracer = otel.Tracer(name)
+	meter  = otel.Meter(name)
+	//logger  = otelslog.NewLogger(name)
 )
 
 type backendServer struct {
@@ -17,7 +32,30 @@ type backendServer struct {
 }
 
 func (s *backendServer) Compute(ctx context.Context, req *pb.ComputeRequest) (*pb.ComputeResponse, error) {
-	return &pb.ComputeResponse{Result: "Hello " + req.Payload + "!"}, nil
+	span := trace.SpanFromContext(ctx)
+	span.SetAttributes(attribute.String("req.payload", req.Payload))
+	msg, err := generateMessage(ctx, req.Payload)
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error")
+	}
+	return &pb.ComputeResponse{Result: msg}, nil
+}
+
+func generateMessage(ctx context.Context, s string) (string, error) {
+	ctx, span := tracer.Start(ctx, "generateMessage")
+	defer span.End()
+
+	if s == "heavy" {
+		span.AddEvent("starting heavy computation ")
+		time.Sleep(2 * time.Second)
+		span.AddEvent("finish heavy computation")
+	}
+
+	if s == "internal error" {
+		return "", errors.New("error")
+	}
+
+	return "Hello " + s + "!", nil
 }
 
 func main() {
--- cmd/greeter/main.go
+++ cmd/greeter/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
 	"net"
 
 	pb "example.com/go-otel-demo/proto"
 	"example.com/go-otel-demo/telemetry"
 	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
 )
 
 type gatewayServer struct {
@@ -19,12 +24,61 @@ type gatewayServer struct {
 }
 
 func (s *gatewayServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloReply, error) {
+
+	err := validateRequest(req)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "name should be non-empty")
+	}
+
 	bReq := &pb.ComputeRequest{Payload: req.Name}
 	bResp, err := s.backendClient.Compute(ctx, bReq)
 	if err != nil {
 		return nil, err
 	}
 	return &pb.HelloReply{Message: bResp.Result}, nil
+}
+
+func validateRequest(req *pb.HelloRequest) error {
+	if req.GetName() == "" {
+		return errors.New("empty name")
+	}
+	return nil
+}
+
+func TraceIDUnaryServerInterceptor() grpc.UnaryServerInterceptor {
+	return func(
+		ctx context.Context,
+		req interface{},
+		info *grpc.UnaryServerInfo,
+		handler grpc.UnaryHandler,
+	) (interface{}, error) {
+		resp, err := handler(ctx, req)
+
+		if sc := trace.SpanContextFromContext(ctx); sc.IsValid() {
+			md := metadata.Pairs("trace-id", sc.TraceID().String())
+			grpc.SetTrailer(ctx, md)
+		}
+		return resp, err
+	}
+}
+
+func TraceIDStreamServerInterceptor() grpc.StreamServerInterceptor {
+	return func(
+		srv interface{},
+		ss grpc.ServerStream,
+		info *grpc.StreamServerInfo,
+		handler grpc.StreamHandler,
+	) error {
+		ctx := ss.Context()
+
+		err := handler(srv, ss)
+
+		if sc := trace.SpanContextFromContext(ctx); sc.IsValid() {
+			md := metadata.Pairs("trace-id", sc.TraceID().String())
+			ss.SetTrailer(md)
+		}
+		return err
+	}
 }
 
 func main() {
@@ -59,6 +113,8 @@ func main() {
 
 	srv := grpc.NewServer(
 		grpc.StatsHandler(otelgrpc.NewServerHandler()),
+		grpc.ChainUnaryInterceptor(TraceIDUnaryServerInterceptor()),
+		grpc.ChainStreamInterceptor(TraceIDStreamServerInterceptor()),
 	)
 	pb.RegisterGreeterServer(srv, &gatewayServer{backendClient: backendClient})
 	reflection.Register(srv)
